d976cc14324e4f2cfb47b1d62e3c6cf821a3fb80
liguge <liguge741741@gmail.com>
2022-04-15 10:53:58 +0800

    Create Causal_Dilation_Conv1d.py

---
+++ Causal_Dilation_Conv1d.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class CausalConv1d(nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+
+        self.__padding = (kernel_size - 1) * dilation
+
+    def forward(self, input):
+        return super(CausalConv1d, self).forward(F.pad(input, (self.__padding, 0)))
+
+if __name__ == '__main__':
+    input = torch.from_numpy(np.ones((3, 2, 5))).float()
+    CaConv1d = CausalConv1d(in_channels=2, out_channels=6, kernel_size=2, dilation=1)
+    out = CaConv1d(input)
+    print(out.size())
